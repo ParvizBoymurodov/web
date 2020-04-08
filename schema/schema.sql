133da6e8f73a4a041bf2676ce958ec9960c534f1
ParvizBoymurodov <parvizboymurodov@mail.ru>
2020-04-08 19:14:17 +0300

    dbdbdb

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,12 +1,3 @@
-create table if not exists burgers(
-id bigserial primary key ,
-name text not null,
-price int,
-remove bool
-);
-
-
-drop table burgers
 
 create table if not exists burgers(
                                       id bigserial primary key ,
